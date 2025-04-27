7a9818eb6fd81afaf78932d7fb6108f21a23ea93
AliciaGY <agyspam3@gmail.com>
2025-04-27 03:39:59 +0200

    app route

--- greenlake-pgd/app.ts
+++ greenlake-pgd/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { askController } from '../greenlake-pgd/controllers/ask.controller.ts';
+import { askController } from './controllers/ask.controller';
 
 const app = express();
 app.use(express.json());
