b1fc69dc2f0dc2105e50244a8331e13c33b8e112
AliciaGY <agyspam3@gmail.com>
2025-04-27 03:37:45 +0200

    mcp1

--- greenlake-pgd/app.ts
+++ greenlake-pgd/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { askController } from './controllers/ask.controller';
+import { askController } from '../greenlake-pgd/controllers/ask.controller.ts';
 
 const app = express();
 app.use(express.json());
